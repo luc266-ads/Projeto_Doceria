7c5df31b4842853902ff366b5c56de977f0499a9
lucas <lucascassian.lc@gmail.com>
2026-02-05 10:00:39 -0300

    API do sistema Usuario e ADM_ etapa 01

--- backEnd/server.js
+++ backEnd/server.js
@@ -4,5 +4,5 @@ const app = require('./app');
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando em http://localhost:${PORT}/users`);
 });
