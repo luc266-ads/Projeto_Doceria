057dfd9d0a8b94cf37cacd8a6a743535e82a17e0
lucas <lucascassian.lc@gmail.com>
2026-02-09 09:01:09 -0300

    Ajeitando a interface inicial

--- front/src/app/inicio/tab1.page.ts
+++ front/src/app/inicio/tab1.page.ts
@@ -3,6 +3,14 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, Io
 import { RouterLink } from '@angular/router';
 import { NgFor } from '@angular/common';
 
+interface Produto {
+  nome: string;
+  descricao: string;
+  preco: string;
+  icone: string;
+  adicionado?: boolean;
+}
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -15,10 +23,57 @@ import { NgFor } from '@angular/common';
   ],
 })
 export class Tab1Page {
-  destaques = [
-    { nome: 'Bolo Red Velvet', descricao: 'Bolo cremoso com cream cheese', preco: '89,90', icon: 'nutrition' },
-    { nome: 'Brownie', descricao: 'Chocolate 70% cacau', preco: '12,90', icon: 'square' },
-    { nome: 'Torta de Limão', descricao: 'Cremosa e refrescante', preco: '45,00', icon: 'pie-chart' },
-    { nome: 'Cupcake', descricao: 'Diversos sabores', preco: '8,90', icon: 'sparkles' },
+  menuAberto = false;
+
+  categorias = [
+    { nome: 'Combos', icone: '🎂' },
+    { nome: 'Bolos', icone: '🍰' },
+    { nome: 'Tortas', icone: '🥧' },
+    { nome: 'Doces', icone: '🧁' }
   ];
+
+  produtos: Produto[] = [
+    {
+      nome: 'Bolo Red Velvet',
+      descricao: 'Bolo cremoso com cream cheese',
+      preco: '89,90',
+      icone: '🍰'
+    },
+    {
+      nome: 'Brownie',
+      descricao: 'Chocolate 70% cacau',
+      preco: '12,90',
+      icone: '🍫'
+    },
+    {
+      nome: 'Torta de Limão',
+      descricao: 'Cremosa e refrescante',
+      preco: '45,00',
+      icone: '🍋'
+    },
+    {
+      nome: 'Cupcake',
+      descricao: 'Diversos sabores',
+      preco: '8,90',
+      icone: '🧁'
+    }
+  ];
+
+  toggleMenu() {
+    this.menuAberto = !this.menuAberto;
+  }
+
+  fecharMenu() {
+    this.menuAberto = false;
+  }
+
+  adicionar(produto: Produto) {
+    produto.adicionado = true;
+
+    setTimeout(() => {
+      produto.adicionado = false;
+    }, 2000);
+  }
 }
+
+
